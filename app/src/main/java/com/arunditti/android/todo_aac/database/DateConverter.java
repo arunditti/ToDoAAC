4f00f66027f93213262219e64ea1c6960a886516
arunditti <arunditti@gmail.com>
2018-06-07 13:28:31 -0400

    Created Room Database

---
+++ app/src/main/java/com/arunditti/android/todo_aac/database/DateConverter.java
@@ -0,0 +1,21 @@
+package com.arunditti.android.todo_aac.database;
+
+import android.arch.persistence.room.TypeConverter;
+
+import java.util.Date;
+
+/**
+ * Created by arunditti on 6/7/18.
+ */
+
+public class DateConverter {
+    @TypeConverter
+    public static Date toDate(Long timestamp) {
+        return timestamp == null ? null : new Date(timestamp);
+    }
+
+    @TypeConverter
+    public static Long toTimestamp(Date date) {
+        return date == null ? null : date.getTime();
+    }
+}
